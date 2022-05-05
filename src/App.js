d6db93eb8fe1670f675555a47a8f3303f2511210
dragonfire124 <whitescorpion26@gmail.com>
2022-05-05 17:57:25 -0500

    3 files added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import Calculator from "./components/Calculator/"
-
+import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator/';
+import Header from './componenents/Header';
+import FormTips from './componenents/FormTips';
+import  GeneralInput  from './componenents/GeneralInput';
+import Result from './componenents/Result';
+import Tips from './componenents/Tips';
 
-
-export default function App(){
-    return(
-        <Calculator/>
-    )
+export default function App() {
+  return <Calculator />;
 }
 
+const TipCalc = (amount, tipsAmount = 15) => {
+  return amount * (tipsamount / 100);
+};
+
+const TotalCalc = (amount, tipsAmount) => {
+  return parseFloat(amount) + parseFloat(tipsAmount);
+};
+
+const TipsByPerson = () => {
+  return parseFloat(tipsAmount) / parseFloat(PersonNumber);
+};
+
+const AmountByPerson = (amount, PersonNumber) => {
+  return amount / PersonNumber;
+};
+
+return;
+<div className="App">
+  <div className="App-container">
+    <div className="App-content">
+      <Header />
+      <Calculator>
+        <FormTips>
+          <GeneralInput label="Bill" icon="dolar" />
+          <Tips />
+          <GeneralInput label="Number of people" icon="people" />
+        </FormTips>
+        <Result></Result>
+      </Calculator>
+    </div>
+  </div>
+</div>;
---
+++ src/components/GeneralInput/GeneralInput.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const GeneralInput = ({ label, icon }) => {
+  return <div className="GeneralInput"></div>;
+};
