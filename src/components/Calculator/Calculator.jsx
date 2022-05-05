0b5d833576ae622a9fda1a7188b75307a69ebb9a
dragonfire124 <whitescorpion26@gmail.com>
2022-05-05 15:05:26 -0500

    1 file modified

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './Calculator.scss'
-import Result from "../Result"
-import FormTips from '../FormTips'
+import React from 'react';
+import './Calculator.scss';
+import Result from '../Result';
+import FormTips from '../FormTips';
 
-const Calculator =()=>{
-    return(
-        <>
-         <Result/>
-         <FormTips/>
-         </>
-    )
-}
+const Calculator = () => {
+  return (
+    <div className="Calculator">
+      <Result />
+      <FormTips />
+    </div>
+  );
+};
 
-export default Calculator
+export default Calculator;
