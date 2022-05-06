125bec1656cb34f884852b134f29c205cb8b1e2f
dragonfire124 <whitescorpion26@gmail.com>
2022-05-05 22:21:07 -0500

    1 file modified, 2 added

--- src/components/Tips/Tips.jsx
+++ src/components/Tips/Tips.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import React from 'react';
+import { TipsButton } from '..TipsButton';
+
+const TipsNumber = [{}, {}, {}, {}];
+
 const Tips = () => {
   return (
     <div className="Tips">
