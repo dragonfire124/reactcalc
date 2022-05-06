04de01d0a8bda8343d1e650c1c11cb672da20aa0
dragonfire124 <whitescorpion26@gmail.com>
2022-05-06 15:11:37 -0500

    4 files modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Header.scss';
 
 const Header = () => {
-  return <div className="Header"></div>;
+  return (
+    <div className="Header">
+      <h2>SPLI</h2>
+
+      <h2>TTER</h2>
+    </div>
+  );
 };
 
 export default Header;
