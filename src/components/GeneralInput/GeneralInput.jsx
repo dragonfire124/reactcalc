eb80a2f036ad6ef0e97657dfa54e8b93120bedc0
dragonfire124 <whitescorpion26@gmail.com>
2022-05-05 19:19:30 -0500

    1 file modified

--- src/components/GeneralInput/GeneralInput.jsx
+++ src/components/GeneralInput/GeneralInput.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomInput from './CustomInput';
+import CustomInput from '../CustomInput';
 
 import './GeneralInput.scss';
 const GeneralInput = ({ label, icon }) => {
