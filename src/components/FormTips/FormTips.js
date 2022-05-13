5d21ad04185ea6126e63f8ead0c3cad80ac1102e
dragonfire124 <whitescorpion26@gmail.com>
2022-05-13 14:38:45 -0500

    1 file modified

--- src/components/FormTips/FormTips.js
+++ src/components/FormTips/FormTips.js
@@ -3,7 +3,12 @@ import './FormTips.scss';
 import GeneralInput from '../GeneralInput';
 
 const FormTips = ({ children }) => {
-  return <div FormTips>{children}</div>;
+  return (
+    <div className="FormTips">
+      <div className="FormTips-Container">
+        <div className="FormTips-Content">{children}</div></div>
+    </div>
+  );
 };
 
 export default FormTips;
